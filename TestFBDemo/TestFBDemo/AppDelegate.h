ddc8f75aaa8fc5e105d7bdbf8f8a9eccda7f4051
tangshunzhi <1066975221@qq.com>
2015-12-23 10:03:13 +0800

    init

---
+++ TestFBDemo/TestFBDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestFBDemo
+//
+//  Created by tang on 15/12/23.
+//  Copyright © 2015年 shunzhitang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestFBDemo/TestFBDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestFBDemo
+//
+//  Created by tang on 15/12/23.
+//  Copyright © 2015年 shunzhitang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
